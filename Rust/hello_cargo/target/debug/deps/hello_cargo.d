bff85ec40ecc220f8c50bb364573680629d5aa13
BrMacCath <brian.collery@colostate.edu>
2024-08-20 09:29:11 -0600

    First project with cargo.

---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\brian\LearnProgramming\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src/main.rs
+
+C:\Users\brian\LearnProgramming\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\brian\LearnProgramming\Rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\brian\LearnProgramming\Rust\hello_cargo\src\main.rs
